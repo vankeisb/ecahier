b8fe9f8d3154a458222f783ead781ebcb12ca34b
Remi Vankeisbelck <remi@rvkb.com>
2012-02-04 18:18:07 +0100

    entries ordering

--- src/main/groovy/com/rvkb/ecahier/woko/EcahierStore.groovy
+++ src/main/groovy/com/rvkb/ecahier/woko/EcahierStore.groovy
@@ -8,6 +8,7 @@ import org.hibernate.criterion.Restrictions
 import org.hibernate.Criteria
 import org.hibernate.criterion.MatchMode
 import woko.persistence.ListResultIterator
+import org.hibernate.criterion.Order
 
 class EcahierStore extends HibernateCompassStore {
 
@@ -15,6 +16,35 @@ class EcahierStore extends HibernateCompassStore {
 
     EcahierStore(List<String> packageNames) {
         super(packageNames)
+    }
+
+    // overriden to add orderBy to criteria...
+    @Override
+    ResultIterator list(String className, Integer start, Integer limit) {
+        Class clazz = getMappedClass(className);
+        int s = start==null ? 0 : start;
+        int l = limit==null ? -1 : limit;
+        if (clazz==null) {
+          return new ListResultIterator(Collections.emptyList(), s, l, 0);
+        } else {
+          Criteria crit = getSession().
+            createCriteria(clazz).
+            setFirstResult(s).
+            addOrder(Order.desc("creationDate"));
+          if (l!=-1) {
+            crit.setMaxResults(l);
+          }
+
+          // TODO optimize with scrollable results ?
+          List objects = crit.list();
+
+          // compute total count
+          String mappedClassName = getClassMapping(clazz);
+          String query = new StringBuilder("select count(*) from ").append(mappedClassName).toString();
+          Long count = (Long)getSession().createQuery(query).list().get(0);
+
+          return new ListResultIterator(objects, s, l, count.intValue());
+        }
     }
 
     ResultIterator<User> getCompletionUsers(String criteria, Integer start, Integer limit) {
