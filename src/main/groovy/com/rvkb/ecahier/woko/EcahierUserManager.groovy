007f2353350af05ac757b185c06e91dcc968d830
boissonnat <alexis.boissonnat@gmail.com>
2012-02-10 19:01:36 +0100

    Use groovy for everything

---
+++ src/main/groovy/com/rvkb/ecahier/woko/EcahierUserManager.groovy
@@ -0,0 +1,23 @@
+package com.rvkb.ecahier.woko
+
+import woko.ext.usermanagement.core.DatabaseUserManager
+import com.rvkb.ecahier.model.User
+import woko.hibernate.HibernateStore
+import woko.ext.usermanagement.hibernate.HibernateUserManager
+
+
+class EcahierUserManager extends HibernateUserManager {
+
+    EcahierUserManager(HibernateStore hibernateStore) {
+        super(hibernateStore, User.class)
+    }
+
+    @Override
+    DatabaseUserManager createDefaultUsers() {
+        super.createDefaultUsers()
+        createUser("kakou", "kakou", Arrays.asList("educ"))
+        createUser("david", "david", Arrays.asList("educ"))
+        createUser("sofiane", "sofiane", Arrays.asList("usager"))
+        return this
+    }
+}
