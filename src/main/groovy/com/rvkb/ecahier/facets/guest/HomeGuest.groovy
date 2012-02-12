1923e4df14397676cda5ae2ac33a0b39892fbb86
boissonnat <alexis.boissonnat@gmail.com>
2012-02-12 11:10:21 +0100

    Use Redirect instead of Forward

--- src/main/groovy/com/rvkb/ecahier/facets/guest/HomeGuest.groovy
+++ src/main/groovy/com/rvkb/ecahier/facets/guest/HomeGuest.groovy
@@ -2,13 +2,16 @@ package com.rvkb.ecahier.facets.guest
 
 import net.sourceforge.jfacets.annotations.FacetKey
 import woko.facets.builtin.all.HomeImpl
+import net.sourceforge.stripes.action.Resolution
+import net.sourceforge.stripes.action.ActionBeanContext
+import net.sourceforge.stripes.action.RedirectResolution
 
 @FacetKey(name="home", profileId="eguest")
 class HomeGuest extends HomeImpl {
 
     @Override
-    String getPath() {
-        'WEB-INF/woko/jsp/login.jsp'
+    Resolution getResolution(ActionBeanContext abc) {
+        return new RedirectResolution('/login')
     }
 
 
