96c10d97a1b7b8f54b6c176d41779351db60e70c
Remi Vankeisbelck <remi@rvkb.com>
2012-02-16 10:53:02 +0100

    populate

--- src/main/groovy/com/rvkb/ecahier/facets/Populate.groovy
+++ src/main/groovy/com/rvkb/ecahier/facets/Populate.groovy
@@ -22,6 +22,21 @@ class Populate extends BaseResolutionFacet {
         def sandy = um.getUserByUsername('sandy')
         def david = um.getUserByUsername('david')
 
+        (1..1000).each { i ->
+            def b = i % 2 == 0
+            s.save(new Entry([
+              createdBy: b ? kakou : david,
+              participants: b ? [sandy] : [sofiane, kakou],
+              text: """Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+est, qui dolorem ipsum quia dolor generated$i sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
+tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
+vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"""]))
+        }
 
         s.save(new Entry([
           createdBy: kakou,
